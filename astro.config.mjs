da8646861e507b4aa0dded572f8f1671a2f94ce1
Clover Chapman <clo@cloverseeds.co.uk>
2023-10-15 18:22:50 +0100

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,63 @@
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      // apiOptions: {
+      //   cache: { clear: "auto", type: "memory" },
+      // },
+      components: {
+        // Content types
+        page: "storyblok/Page",
+        feature: "storyblok/Feature",
+        eventList: "storyblok/EventList",
+        event: "storyblok/Event",
+        teamMembersList: "storyblok/TeamMembersList",
+        teamMember: "storyblok/TeamMember",
+        featuredEvents: "storyblok/FeaturedEvents",
+
+        // Heroes
+        hero: "storyblok/Heroes/Hero",
+
+        //components
+        button: "storyblok/Button",
+      },
+      apiOptions: {
+        region: "",
+      },
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon({
+      include: {
+        tabler: ["*"],
+        "flat-color-icons": [
+          "template",
+          "gallery",
+          "approval",
+          "document",
+          "advertising",
+          "currency-exchange",
+          "voice-presentation",
+          "business-contact",
+          "database",
+        ],
+      },
+    }),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--aw-color-accent)",
+        secondary: "var(--shn-color-secondary)",
+        accent: "var(--shn-color-accent)",
+        default: "var(--shn-color-text-default)",
+      },
+      fontFamily: {
+        sans: ["var(--shn-font-sans)", ...defaultTheme.fontFamily.sans],
+        serif: ["var(--shn-font-serif)", ...defaultTheme.fontFamily.serif],
+        heading: ["var(--shn-font-heading)", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
